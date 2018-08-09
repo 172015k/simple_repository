bf97e3543360ac7f68754c57a48aab753a301725
JaeKeun Song <172015k@gmail.com>
2018-08-09 09:14:36 +0900

    comment

---
+++ MySampleTab/app/src/main/java/com/example/song/mysampletab/Fragment1.java
@@ -0,0 +1,22 @@
+package com.example.song.mysampletab;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.activity_fragment1,container,false);
+        return rootView;
+    }
+
+}
---
+++ MySampleTab/app/src/main/java/com/example/song/mysampletab/Fragment2.java
@@ -0,0 +1,20 @@
+package com.example.song.mysampletab;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.activity_fragment2,container,false);
+        return rootView;
+    }
+}
